77c8c8d5d640c2ce43886aacfd365f1a466b3eff
yamabouz <52478552+yamabouz@users.noreply.github.com>
2025-07-02 12:55:40 +0900

    新規作成

---
+++ src/test/java/jp/spring/artisans/ArtisansApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.spring.artisans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtisansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
