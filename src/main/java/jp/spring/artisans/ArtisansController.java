fcf87a8df0145671dbe39de8f166e5b330928406
yamabouz <52478552+yamabouz@users.noreply.github.com>
2025-07-02 17:49:39 +0900

    フォーム作成

---
+++ src/main/java/jp/spring/artisans/ArtisansController.java
@@ -0,0 +1,37 @@
+package jp.spring.artisans;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArtisansController {
+	
+////	ホーム画面
+//	@GetMapping("/")
+//	public String index() {
+//		return "index";
+//	}
+////	フォーム画面
+//	@GetMapping("form")
+//	public String form() {
+//		return "form";
+//	}
+//	
+//	フォームデータのやりとり
+	@PostMapping("/formresult")
+	public String showIndex(@ModelAttribute ArtisansForm form,Model model) {
+		model.addAttribute("name", form.getName());
+	    model.addAttribute("mail", form.getMail());
+	    model.addAttribute("age", form.getAge());
+	    model.addAttribute("contents", form.getContents());
+	    return "formresult";
+	}
+	@GetMapping("/form")
+	public String showForm(Model model) {
+		model.addAttribute("artisansForm", new ArtisansForm());
+		return "form";
+	}
+}
---
+++ src/main/java/jp/spring/artisans/ArtisansForm.java
@@ -0,0 +1,17 @@
+package jp.spring.artisans;
+
+import lombok.Data;
+
+
+@Data
+public class ArtisansForm {
+
+	private String name;
+	
+	private String mail;
+	
+	private Integer age;
+	
+	private String contents;
+
+}
